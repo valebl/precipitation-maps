5fb57160a9f5ec27c4912b0d319e5bcc3d2dc18e
valebl <valebl@DESKTOP-06R2CFC.localdomain>
2022-07-15 12:26:23 +0200

    fix preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -12,40 +12,42 @@ import xarray as xr
 
 def preprocessing_input(input_path, output_path, log_path, lat_dim, lon_dim, n_levels, time_dim, n_variables, year_start, year_end):
 
-    database = np.zeros((lat_dim, lon_dim, n_levels, time_dim, n_variables)) # '+ 1' is for the target pr value
-    # output = np.zeros((num_partitions-1, period_of_influence*2, lat_dim, lon_dim, n_levels*n_variables + 1)) # '+ 1' is for the target pr value
-    l_start = 0
-    for v in ['q', 't', 'u', 'v', 'z']:
-        time_start = 0
-        for year in range(year_start, year_end +1):
+    time_start = 0
+    for year in range(year_start, year_end +1):
+        with xr.open_dataset(input_path + f'q_{year}.nc') as file:
+            time_year_dim = len(file.time)
+            database = np.zeros((lat_dim, lon_dim, n_levels, time_year_dim, n_variables))
+        v_idx = 0
+        for v in ['q', 't', 'u', 'v', 'z']:
             with xr.open_dataset(input_path + f'{v}_{year}.nc') as file:
-                time_year_dim = len(file.time)
                 data = file[v].values
                 s = data.shape
                 data = data.reshape(s[2], s[3], s[1], s[0])         # (lat_dim, lon_dim, n_levels, time_year_dim)
-                database[:,:,:,time_start:time_start+time_year_dim,l_start:l_start+5] = data
-                time_start += time_year_dim
+                database[:,:,:,:,v_idx] = data
                 with open(log_path+'log_input.txt', 'a') as f:
                     f.write(f'\nPreprocessing {v}_{year}.nc, time indexes from {time_start} to {time_start+time_year_dim}.')
-        l_start += 5
+            v_idx += 1
+
         with open(log_path+'log_input.txt', 'a') as f:
-            f.write(f'\nFinished preprocessing of {v}.')
-    
-    with open(log_path+'log_input.txt', 'a') as f:
-        f.write(f'\nStarting to write the output file.')
+            f.write(f'\nFinished preprocessing of year {year}.')
 
-    with h5py.File(output_path+'input.hdf5', 'w') as f:
-        f.create_dataset('input', database.shape, data=database,
-            compression="gzip",  maxshape=(database.shape[0], database.shape[1], database.shape[2], None, database.shape[4]))
-
-        # else:
-                #    with h5py.File(f'{work_dir}/PREPROCESSED/preprocessed_data.hdf5', 'a') as f:
-                        #        f["data"].resize((f["data"].shape[0] + output_array.shape[0]), axis = 0)
-                                #        f["data"][-output_array.shape[0]:] = output_array
-
-
-    with h5py.File(output_path+'input.hdf5', 'w') as f:
-        f.create_dataset('input', database.shape, data=database)
+        with open(log_path+'log_input.txt', 'a') as f:
+            f.write(f'\nStarting to write the output file (year={year}).')
+        
+        if year == year_start:
+            with h5py.File(output_path+'input.hdf5', 'w') as f:
+                f.create_dataset('input', database.shape, data=database,
+                    compression="gzip",  maxshape=(database.shape[0], database.shape[1], database.shape[2], None, database.shape[4]))
+        else:
+            with h5py.File(output_path+'input.hdf5','a') as f:
+                f['input'].resize((f['input'].shape[3] + database.shape[3]), axis = 3)
+                f['input'][:,:,:,-database.shape[3]:,:] = database
+        
+        with open(log_path+'log_input.txt', 'a') as f:
+            f.write(f'\Output file written (year={year}).')
+        
+        time_start += time_year_dim
+        del database
 
     with open(log_path+'log_input.txt', 'a') as f:
         f.write(f'\nPreprocessing finished.')
