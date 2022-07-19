fdef4561975b47c2f3ffa369e06905a082147674
valebl <kiwi5.vb@gmail.com>
2022-07-19 17:57:33 +0200

    minor updates

--- preprocessing_gripho.py
+++ preprocessing_gripho.py
@@ -6,7 +6,7 @@ TIME_DIM = 140256
 LON_MIN = 6.5
 LON_MAX = 18.75
 LAT_MIN = 36.5
-LAT_MAX = 47.25
+LAT_MAX =  47.25
 INTERVAL = 0.25
 
 def select_from_gripho(lon_min, lon_max, lat_min, lat_max, lon, lat, pr):
@@ -37,7 +37,9 @@ if __name__ == '__main__':
         j = 0
         for lat_era5 in lat_era5_list:
             selected_lon, selected_lat, selected_pr = select_from_gripho(lon_era5, lon_era5+INTERVAL, lat_era5, lat_era5+INTERVAL, lon, lat, pr)
-            target[i, j] = {'longitude' : selected_lon, 'latitude': selected_lat, 'pr': selected_pr, 'lon_era_5': lon_era5, 'lat_era_5': lat_era5}
-    
-with open('target.pkl', 'wb') as f:
+            target[i, j] = {'longitude' : selected_lon, 'latitude': selected_lat, 'pr': selected_pr, 'lon_era5': lon_era5, 'lat_era5': lat_era5}
+            j += 1
+        i += 1
+
+with open('/m100_work/ICT22_ESP_0/vblasone/PREPROCESSED/target.pkl', 'wb') as f:
     pickle.dump(target, f)
