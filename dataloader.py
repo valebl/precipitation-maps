949e90ad294c627418f2d8950a32c2270ef59b1e
valebl <valebl@DESKTOP-06R2CFC.localdomain>
2022-07-20 11:23:46 +0200

    fix database and custom collate

--- dataloader.py
+++ dataloader.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import pickle
 from torch.utils.data import Dataset
-
+from torch.utils.data._utils.collate import default_convert
 
 class Clima_dataset(Dataset):
 
@@ -15,27 +15,33 @@ class Clima_dataset(Dataset):
 
     def __init__(self, input_path, target_path, **kwargs):
         super().__init__()
-        self.pad = 2
+        self.PAD = 2
+        self.TIME = 140256 - 24
         self.input_path = input_path
         self.target_path = target_path
         self.input_data, self.target_data = self._load_data_into_memory(self.input_path, self.target_path)
         self.lon_size = self.input_data.shape[0]
         self.lat_size = self.input_data.shape[1]
-        self.idxs = np.array([[(i,j) for j in range(self.pad,self.lat_size-(self.pad+1))] for i in range(self.pad,self.lon_size-(self.pad+1))])
-        self.idxs = self.idxs.reshape(-1, self.idxs.shape[-1])
+        self.idxs_space = np.array([[(i,j) for j in range(self.PAD,self.lat_size-(self.PAD+1))] for i in range(self.PAD,self.lon_size-(self.PAD+1))])
+        self.idxs_space = self.idxs_space.reshape(-1, self.idxs_space.shape[-1]) # flatten the lon and lat dimensions into a single dimension
+        self.idxs = np.tile(self.idxs_space, (self.TIME,1))
 
     def __len__(self):
-        return self.idxs.shape[0]
+        return self.idxs_space.shape[0] * self.TIME
 
     def __getitem__(self, idx):
-        lon_idx = self.idxs[idx][0]
-        lat_idx = self.idxs[idx][1]
-        input = self.input_data[lon_idx-self.pad:lon_idx+self.pad+2, lat_idx-2:lat_idx+self.pad+2, :, :]
-        target = self.target_data[lon_idx-2, lat_idx-2]
+        idx_time = idx // self.idxs_space.shape[0] + 24
+        idx_space = idx % self.idxs_space.shape[0]        
+        lon_idx = self.idxs_space[idx_space][0]
+        lat_idx = self.idxs_space[idx_space][1]
+        input = self.input_data[lon_idx-self.PAD:lon_idx+self.PAD+2, lat_idx-2:lat_idx+self.PAD+2, :, idx_time-24:idx_time+1]
+        target = self.target_data[lon_idx-2, lat_idx-2].copy()
+        target['pr'] = self.target_data[lon_idx-2, lat_idx-2]['pr'][idx_time,:].copy()
         return input, target
-        
+
 def custom_collate_fn(batch):
-    data = [item[0] for item in batch]
+    data = np.array([item[0] for item in batch])
     target = [item[1] for item in batch]
-    data = torch.tensor(data, dtype=torch.float32)
+    data = default_convert(data)
+    target = target
     return [data, target]
